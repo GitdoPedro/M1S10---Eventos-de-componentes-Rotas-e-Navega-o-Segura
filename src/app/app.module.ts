e86e348faebc58e1441930c9f00c03f85c4edf2b
GitdoPedro <emailpedrolima@gmail.com>
2023-01-10 16:27:37 -0300

    exercicio 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
@@ -8,6 +10,28 @@ import { HeaderComponent } from './components/header/header.component';
 import { FiltroComponent } from './components/filtro/filtro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+
+const ROUTES: Routes = [
+  {
+    path: '/',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'home/:lidos/:nao-lidos/:todos',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+
+]
+
 
 @NgModule({
   declarations: [
@@ -21,6 +45,7 @@ import { AboutComponent } from './pages/about/about.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
